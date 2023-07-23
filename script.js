23a2c6e3b9d81e6049eee557ab0a832258b2e3e2
Priyanshu885588 <priyanshumandani978@gmail.com>
2023-07-23 17:46:57 +0530

    home page

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.querySelector(".search-btn");
 searchBtn.addEventListener("click", () => {
-    searchInput.style.display = "block";
-    searchBtn.style.borderBottom="1px solid black"
+  searchInput.style.display = "block";
+  searchBtn.style.borderBottom = "1px solid black";
 });
+let tf = false;
+const hambtn = document
+  .querySelector(".ham-btn")
+  .addEventListener("click", () => {
+    if (!tf) {
+      document.getElementById("nav-items").style.display = "flex";
+      tf=true;
+    }
+    else {
+        document.getElementById("nav-items").style.display = "none";
+        tf=false;
+    }
+
+  });
